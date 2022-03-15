f396cc89fc8ce7be9a243813caffe023ce372e34
ibrahimengin <ibrahim.engin04@hotmail.com>
2022-03-15 17:32:25 +0300

    Button design and define in login screen new words translated to turkish

--- app/src/main/java/com/ibrahimengin/was/view/CustomButton.kt
+++ app/src/main/java/com/ibrahimengin/was/view/CustomButton.kt
@@ -1,24 +1,39 @@
 package com.ibrahimengin.was.view
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Alarm
+import androidx.compose.material.icons.filled.Cake
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.WASTheme
 
 @Composable
-fun ButtonTrailingIcon(buttonClickFunction: () -> Unit, buttonText: String) {
-    Button(onClick = buttonClickFunction){
+fun ButtonTrailingIcon(buttonClickFunction: () -> Unit, buttonText: String, iconsInputs: ImageVector,
+                       iconsContentDescription: String, colorInput: Color? = MaterialTheme.colors.primary) {
+    Button(onClick = buttonClickFunction, colors = ButtonDefaults.buttonColors(colorInput!!)){
         Text(buttonText)
+        Spacer(modifier = Modifier.width(2.dp))
+        Icon(imageVector = iconsInputs, iconsContentDescription, modifier = Modifier.size(18.dp))
 
     }
 }
 
 @Composable
-fun ButtonLeadingIcon(buttonClickFunction: () -> Unit, buttonText: String){
-    Button(onClick = buttonClickFunction){
+fun ButtonLeadingIcon(buttonClickFunction: () -> Unit, buttonText: String, iconsInputs: ImageVector,
+                      iconsContentDescription: String, colorInput: Color? = MaterialTheme.colors.primary){
+    Button(onClick = buttonClickFunction, colors = ButtonDefaults.buttonColors(colorInput!!)){
+        Icon(imageVector = iconsInputs, iconsContentDescription, modifier = Modifier.size(18.dp))
+        Spacer(modifier = Modifier.width(2.dp))
         Text(buttonText)
-
     }
 }
 
@@ -26,6 +41,10 @@ fun ButtonLeadingIcon(buttonClickFunction: () -> Unit, buttonText: String){
 @Composable
 fun PreviewButtonDefine(){
     WASTheme {
-        ButtonTrailingIcon({},"")
+        Column {
+            ButtonTrailingIcon({},"BUTTON",Icons.Filled.Alarm,"Button")
+            ButtonLeadingIcon({},"LBUTTON",Icons.Filled.Cake,"")
+        }
+
     }
 }
--- app/src/main/java/com/ibrahimengin/was/view/LoginScreen.kt
+++ app/src/main/java/com/ibrahimengin/was/view/LoginScreen.kt
@@ -1,16 +1,18 @@
 package com.ibrahimengin.was.view
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AppRegistration
+import androidx.compose.material.icons.filled.Login
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +31,19 @@ fun LoginScreen(){
             verticalArrangement = Arrangement.Center) {
             CustomImage(logo,"WAS Logo",250.dp,250.dp)
             CustomOutlinedTextField(username.value, {username.value = it}, stringResource(R.string.username))
-            PasswordField(password.value, {password.value = it}, stringResource(R.string.password))
+            PasswordField(password.value, {password.value = it}, stringResource(R.string.password))//TODO Dark temadaki sorunlar çözülecek
+            Spacer(modifier = Modifier.height(5.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Text(stringResource(R.string.forgotPassword))//TODO clickable-onClick fonksionu eklenecek
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+            Spacer(modifier = Modifier.height(15.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+                ButtonTrailingIcon({}, stringResource(R.string.signup), Icons.Filled.AppRegistration,
+                    stringResource(R.string.signup), Color.Gray)//TODO onClick fonksionu eklenecek
+                ButtonTrailingIcon({}, stringResource(R.string.login), Icons.Filled.Login,
+                    stringResource(R.string.login))//TODO onClick fonksionu eklenecek
+            }
         }
     }
 }
